f190b9ba0491bbf2b8e44a904d7918ef76db6dc2
김도현 <kd8317@gmail.com>
2022-05-26 02:46:27 +0900

    Update card animation to search movies

--- vue/src/views/SearchMovieView.vue
+++ vue/src/views/SearchMovieView.vue
@@ -2,27 +2,31 @@
   <div>
     <div class="search-input">
       <p class="my-3 fs-3 fw-bold">Search</p>
-      <div class="d-flex justify-content-center mb-3">
-        <i class='bx bx-search-alt-2 fa-lg'></i>
-        <input placeholder="  Please enter movie title" class="box" v-model="input" @keyup.enter="clickSearch" type="text">
+      <div class="d-flex justify-content-center align-items-center">
+        <i class='bx bx-search-alt-2 fa-lg mx-2'></i>
+        <input placeholder="Please enter movie title" class="px-4 box" v-model="input" @keyup.enter="clickSearch" type="text">
       </div>
-      <!-- <button @click="clickSearch" class="btn bg-primary search-btn"><div class="search-icon"><i class='bx bx-search-alt-2'></i></div> Search</button> -->
     </div>
     
     <div class="container">
-    <div class="row">
-      <div class="d-flex justify-content-center col-12 col-md-6 col-lg-4 col-xl-3" v-for='movie in searchResponse' :key="movie.id">
-        <router-link :to="{ name: 'detail', params: { moviePk: movie.pk } }">
-        <div class="card mb-3" style="width: 15rem;">
-          <img class="card-img-top" :src="poster_url(movie.poster_path)" alt>
-          <!-- <div class="card-body">
-            <h5 class="card-title">{{ movie.title }}</h5>
-            <p class="card-text">{{ movie.overview }}</p>
-          </div> -->
+      <div class="row">
+        <div class="d-flex justify-content-center col-12 col-md-6 col-lg-4 col-xl-3" v-for='movie in searchResponse' :key="movie.id">
+          <router-link :to="{ name: 'detail', params: { moviePk: movie.pk } }">
+            <div class="a">
+            <div class="card mb-3 screen" style="width: 15rem;">
+              <img class="card-img-top" :src="poster_url(movie.poster_path)" alt="card-img">
+              <div class="card-body">
+                <h5 class="card-title top">{{ movie.title }}</h5>
+                <div class="bottom">
+                  <i class="fa-solid fa-hourglass-end"> {{ movie.runtime }}</i> min.
+                  <div>{{ movie.tagline }}</div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </router-link>
         </div>
-        </router-link>
       </div>
-    </div>
     </div>
   </div>
 </template>
@@ -57,25 +61,66 @@ export default {
 </script>
 
 <style>
+.search-input div {
+  margin-top: 5rem;
+  margin-bottom: 2rem;
+}
 .box {
   width: 800px;
   height: 50px;
   border-radius: 20px;
 }
-/* .search-input {
-  min-height: 400px;
+
+.screen {
+  position: relative;
+  overflow: hidden;
+  height: 400px;
 }
-.search-input input {
-  height: 45px;
+
+.screen .top {
+  position: absolute;
+  bottom:-350%;
+  /* left:30px; */
+  z-index:2;
+  color:#fff;
+  font-size:20px;
+  font-weight: 900;
+  transition: all .35s;
 }
-.search-btn {
-  height: 45px;
-  display: flex;
-  justify-content: center;
+
+.screen .bottom {
+  position: absolute;
+  top:100%;
+  /* left:30px; */
+  z-index:2;
+  color:#fff;
+  font-size:14px;
+  transition: all .35s;
 }
-.search-icon {
-  margin: 0;
-  padding-top: 2px;
-  bottom: 0;
-} */
+
+.screen:hover .top {
+  bottom: 52%;
+}
+
+.screen:hover .bottom {
+  top: 50%;
+}
+
+.screen::after {
+  content: '';
+  display: block;
+  position:absolute;
+  top:0;
+  left:0;
+  width:100%;
+  height: 100%;
+  background: rgba(0,0,0,.5);
+  z-index: 1;
+  opacity: 0;
+  transition: all .35s;
+}
+
+.a:hover .screen::after {
+  opacity: 1;
+}
 </style>
