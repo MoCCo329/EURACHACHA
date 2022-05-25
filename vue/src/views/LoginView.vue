5c078f06808ff50d4e33acad31bb986c138d4b08
donghwan <ldh793@naver.com>
2022-05-25 21:31:49 +0900

    Add style in Login page

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-    여기는 로그인 페이지
+  <div class="box">
+    <div class="my-3 fs-3 fw-bold">Login</div>
     <account-error-list v-if="isAuthError"></account-error-list>
     <form @submit.prevent="login(credentials)">
       <div>
-        <label for="username">username: </label>
-        <input v-model="credentials.username" type="text" id="username" required>
+        <!-- <label for="username">username: </label> -->
+        <input class="w-50 loginInput username" v-model="credentials.username" type="text" id="username" required placeholder="  Username">
       </div>
       <div>
-        <label for="password">password: </label>
-        <input v-model="credentials.password" type="password" id="password" required>
+        <!-- <label for="password">password: </label> -->
+        <input class="w-50 loginInput password" v-model="credentials.password" type="password" id="password" required placeholder="  Password">
       </div>
-      <button type="submit">Login</button>
+      <button class="btn btn-light w-50 mt-3 rounded-pill" type="submit">Login</button>
     </form>
   </div>
 </template>
@@ -43,5 +43,25 @@ export default {
 </script>
 
 <style>
+.box {
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  top: 200px;
+  right: 350px;
+}
 
+.loginInput {
+  height: 50px;
+}
+
+.username {
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+}
+
+.password {
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
 </style>
